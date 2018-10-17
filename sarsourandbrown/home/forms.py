522593abad1cd07c1bb6cb4275189710792895ae
CarnsJalone <Jlamborghini22@gmail.com>
2018-10-17 12:05:30 -0400

    Javascript

--- sarsourandbrown/home/forms.py
+++ sarsourandbrown/home/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
+
     name = forms.CharField(label="Name", required=True)
     name.widget.attrs.update({'class': 'form-control', 'name': 'name', 'placeholder': 'Please Enter Your Name'})
     email = forms.EmailField(widget=forms.EmailInput, label="Email", required=True)
@@ -8,8 +9,5 @@ class ContactForm(forms.Form):
     body = forms.CharField(widget=forms.Textarea, label="Enter Inquiry Below", max_length="250", required=True)
     body.widget.attrs.update({'class': 'form-control', 'name':'email', 'placeholder':'Please Enter A Brief Description of the Inquiry', 'rows': '10'})
 
-    def __init__(self, *args, **kwargs):
-        pass
-    
-    def __str__(self):
-        return '{} posted by {} at {}'.format(self.body, self.name, self.email)
+    # def __str__(self):
+    #     return '{} posted by {} at {}'.format(self.body, self.name, self.email)
